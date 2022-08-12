c7b75f81169403bfe325b574ae3b4d773797fe75
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2022-08-12 09:11:29 -0500

    personajes
    personajes y obstáculos

--- juego_andromeda/roca.java
+++ juego_andromeda/roca.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class piedra here.
+ * Write a description of class roca here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class piedra extends Actor
+public class roca extends Actor
 {
     /**
-     * Act - do whatever the piedra wants to do. This method is called whenever
+     * Act - do whatever the roca wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
