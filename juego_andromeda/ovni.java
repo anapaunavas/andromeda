76649d6d76a7ff76a085361383a0e86302a830ac
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2022-08-12 07:31:42 -0500

    Inicio del juego
    Primeros cambios

---
+++ juego_andromeda/ovni.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ovni here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ovni extends Actor
+{
+    /**
+     * Act - do whatever the ovni wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
