0786a01207d184fd78c721f394594eb6f660cb0d
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2022-08-13 15:29:57 -0500

    clase roca

--- juego_andromeda/roca.java
+++ juego_andromeda/roca.java
@@ -8,6 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class roca extends Actor
 {
+    private int speed;
+    
+    public roca(int v) {
+        speed = v;
+    }
     /**
      * Act - do whatever the roca wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +20,10 @@ public class roca extends Actor
     public void act()
     {
         // Add your action code here.
+        setLocation(getX(), getY() + speed);
+        if( getY() >= getWorld().getHeight() - 1) {
+            MyWorld juego = (MyWorld) getWorld();
+            juego.removeObject(this);
+        }
     }
 }
